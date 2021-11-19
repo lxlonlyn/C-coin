84d953bb2ef0077e5a684b823aac0821f8726a0a
lonlyn <30793496+lxlonlyn@users.noreply.github.com>
2021-11-19 23:22:54 +0800

    Add files via upload

---
+++ ecdsa.py
@@ -0,0 +1,304 @@
+# -*- coding: utf-8 -*-
+from typing import Tuple
+import random
+import struct
+import hashlib
+from .sha256 import my_sha256
+from .sha1 import my_sha1
+import base58
+import secrets
+
+
+# ECDSA 签名机制。私匙 160 比特，公匙为点坐标。
+class ECDSA(object):
+    """
+        ECDSA 类：
+            一、每次创建类的对象时，会生成对应的新的私匙和公匙
+
+            二、可以生成私匙和公匙，并实现私匙、公匙、压缩私匙、压缩公匙、地址间的转化
+
+            三、可以根据私匙给信息签名，同时可以根据公匙验证信息
+
+    """
+    # 生成元及模数
+    g = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, \
+        0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+    p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+    MOD = 0xFFFFFFFFFFFFFFFFFFFFFFFF0123456789ABCEA7
+    # 定义无穷点，GF 的阶
+    INF = (-1, -1)
+    order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+
+    def __init__(self):
+        self.private_key = secrets.randbits(256) % ECDSA.p
+        print("private key:", self.private_key)
+        self.public_key = ECDSA.curve_mul(ECDSA.g, self.private_key)
+
+    @classmethod
+    def quick_pow(cls, x: int, k: int, md: int = p) -> int:
+        res = 1
+        while k:
+            if k % 2 == 1:
+                res = res * x % md
+            x = x * x % md
+            k = k // 2
+        return res % md
+
+    @classmethod
+    def inv(cls, input_num: int, md: int = p) -> int:
+        """
+        返回输入值在模意义下的逆元
+
+        :param input_num: 输入值
+        :param md: 模数，默认为 p
+        :return: 模意义下的逆元
+        """
+        k = md - 2
+        res = 1
+        tmp = input_num % md
+        while k > 0:
+            if k % 2 == 1:
+                res = res * tmp % md
+            tmp = tmp * tmp % md
+            k = k // 2
+        return res % md
+
+    @classmethod
+    def curve_add(cls, p1: Tuple[int, int], p2: Tuple[int, int]) -> Tuple[int, int]:
+        """
+        基于椭圆曲线 y^2=x^3+7 上两点的加法。
+
+        :param p1: 一个点
+        :param p2: 一个点
+        :return: 两点求和结果
+        """
+        # 如果是 0, 0 直接返回另一个
+        if p1 == cls.INF:
+            return p2
+        if p2 == cls.INF:
+            return p1
+
+        # 相加为 INF 处理
+        if p1[0] == p2[0]:
+            if (p1[1] + p2[1]) % cls.p == 0:
+                return cls.INF
+
+        # 计算斜率
+        if (p1[0] == p2[0]) and (p1[1] == p2[1]):
+            k = (3 * p1[0] * p1[0]) % cls.p * cls.inv(2 * p1[1]) % cls.p
+        else:
+            k = (p2[1] - p1[1] + cls.p) % cls.p * \
+                cls.inv((p2[0] - p1[0] + cls.p) % cls.p) % cls.p
+
+        # 计算坐标
+        x3 = (k * k + 2 * cls.p - p1[0] - p2[0]) % cls.p
+        y3 = (k * (p1[0] - x3 + cls.p) + cls.p - p1[1]) % cls.p
+        return x3, y3
+
+    @classmethod
+    def curve_mul(cls, x_in: Tuple[int, int], k: int) -> Tuple[int, int]:
+        """
+        椭圆曲线 y^2=x^3+7 上乘法，基于快速加法实现
+
+        :param x_in: 输入的点 x
+        :param k: 倍数 k
+        :return: 点 kx
+        """
+        r = k
+        res = cls.INF
+        tmp = x_in
+        while r > 0:
+            if r % 2 == 1:
+                res = cls.curve_add(res, tmp)
+            tmp = cls.curve_add(tmp, tmp)
+            r = r // 2
+        return res
+
+    @classmethod
+    def get_wif_from_private_key(cls, input_str: str) -> str:
+        """
+        由未压缩私匙生成压缩私匙 wif
+
+        :param input_str: 未压缩私匙
+        :return: 压缩私匙
+        """
+
+        input_str = '80' + input_str + '01'
+        h = my_sha256(my_sha256(input_str, True, True), True, True)
+        input_str += h[0:8]
+        bits = b''
+        for i in range(0, len(input_str), 2):
+            num = (int(input_str[i], 16) << 4) + (int(input_str[i + 1], 16))
+            bits += struct.pack('<B', num)
+        return base58.b58encode(bits).decode()
+
+    @classmethod
+    def get_private_key_from_wif(cls, input_str: str) -> str:
+        """
+        由压缩私匙 wif 生成私匙
+
+        :param input_str: 压缩私匙
+        :return: 未压缩私匙
+        """
+        bits = base58.b58decode(input_str.encode())
+        bits = bits[1:-5]
+        out_str = ""
+        for i in range(32):
+            out_str += hex(bits[i] // 16)[2:] + hex(bits[i] % 16)[2:]
+        return out_str
+
+    @classmethod
+    def get_public_key_from_private_key(cls, private_key: str, is_compressed: bool = False) -> Tuple[int, int]:
+        """
+        由私匙导出公匙，返回未压缩公匙二元组
+
+        :param private_key: 私匙
+        :param is_compressed: 私匙是否为压缩私匙
+        :return: 未压缩公匙
+        """
+        if private_key[0:2] == "0x":
+            private_key = private_key[2:]
+        if is_compressed:
+            key = int(cls.get_private_key_from_wif(private_key), 16)
+        else:
+            key = int(private_key, 16)
+        return cls.curve_mul(cls.g, key)
+
+    @classmethod
+    def get_compressed_public_key_from_public_key(cls, public_key: Tuple[int, int]) -> str:
+        """
+        由未压缩公匙获得压缩公匙
+
+        :param public_key: 未压缩公匙
+        :return: 压缩公匙
+        """
+        x, y = public_key
+        compressed_key = ""
+        if y % 2 == 0:
+            compressed_key += "02"
+        else:
+            compressed_key += "03"
+        num_str = hex(x)[2:]
+        return compressed_key + num_str
+
+    @classmethod
+    def get_public_key_from_compressed_public_key(cls, compressed_key: str) -> Tuple[int, int]:
+        """
+        由压缩公匙获得未压缩公匙
+
+        :param public_key: 压缩公匙
+        :return: 未压缩公匙
+        """
+        y_flag = int(compressed_key[:2], base=16) % 2
+        _x = int(compressed_key[2:], base=16)
+        x = (_x ** 3 + 7) % ECDSA.p
+
+        def cipolla(x: int) -> int:
+            """
+            利用 Cipolla 求二次剩余
+
+            """
+            class cp:
+                def __init__(self, r: int, i: int) -> None:
+                    self.real = r
+                    self.imag = i
+
+            def cpmul(a: cp, b: cp, w: int) -> cp:
+                ans = cp(0, 0)
+                ans.real = ((a.real * b.real % ECDSA.p + a.imag * b.imag %
+                             ECDSA.p * w % ECDSA.p) % ECDSA.p + ECDSA.p) % ECDSA.p
+                ans.imag = ((a.real * b.imag % ECDSA.p + a.imag *
+                             b.real % ECDSA.p) % ECDSA.p + ECDSA.p) % ECDSA.p
+                return ans
+
+            while True:
+                a = random.randint(1, ECDSA.p - 1)
+                w = ((a * a % ECDSA.p - x) % ECDSA.p + ECDSA.p) % ECDSA.p
+                if ECDSA.quick_pow(w, (ECDSA.p - 1) // 2) == ECDSA.p - 1:
+                    break
+
+            x = cp(a, 1)
+            y = cp(1, 0)
+            b = (ECDSA.p + 1) // 2
+            while b > 0:
+                if b % 2 == 1:
+                    y = cpmul(y, x, w)
+                x = cpmul(x, x, w)
+                b = b // 2
+            return y.real % ECDSA.p
+
+        y = cipolla(x)
+        if y % 2 != y_flag:
+            y = ECDSA.p - y
+        return _x, y
+
+    @classmethod
+    def get_address_from_compressed_public_key(cls, compressed_key: str) -> str:
+        """
+        由压缩公匙生成地址
+
+        :param compressed_key: 压缩公匙
+        :return: 地址
+        """
+        # part 1: 对 33 位压缩公匙 -> sha256 -> ripemd160
+        sha = my_sha256(compressed_key, True, True)
+        hash160 = hashlib.new("ripemd160", int(
+            sha, base=16).to_bytes(length=32, byteorder='big'))
+        hash160 = hash160.hexdigest()
+        hash160 = "00" + hash160
+        # part 2: 校验码由 part 1 两次 hash 得到
+        verify_code = my_sha256(my_sha256(hash160, True, True), True, True)[:8]
+        sum_str = hash160 + verify_code
+        bits = b''
+        for i in range(0, len(sum_str), 2):
+            num = (int(sum_str[i], 16) << 4) + (int(sum_str[i + 1], 16))
+            bits += struct.pack('<B', num)
+        return base58.b58encode(bits).decode()
+
+    @classmethod
+    def gen_signature(cls, input_str: str, key: int) -> Tuple[int, int]:
+        """
+        根据私匙生成签名
+
+        生成过程：
+            一. 产生 160 比特随机数 k
+
+            二. 计算 P = k * g
+
+            三. P 的横坐标即为 R
+
+            四. 利用 sha1 计算原信息哈希值，得到 160 位整数 z
+
+            五. 计算 S = k^(-1) * (z + private_key * R) % p
+
+            六. 签名 R,S 一共是 320 比特
+
+        :param input_str: 待签名的信息
+        :param key: 私匙，整数格式
+        :return: 签名 R,S
+        """
+        k = random.randrange(2 ** 150, 2 ** 160, 1)
+        P = cls.curve_mul(cls.g, k)
+        R = P[0]
+        z = int(my_sha1(input_str), 16)
+        S = cls.inv(k, cls.order) * ((z + key * R) % cls.order) % cls.order
+        return R, S
+
+    @classmethod
+    def verify_signature(cls, input_str: str, key: Tuple[int, int], sign: Tuple[int, int]) -> bool:
+        """
+        根据公匙验证签名与信息
+
+        :param input_str: 待验证的信息
+        :param key: 公匙
+        :param sign: 待验证的签名
+        :return: 若签名有效则返回 True，否则为 False
+        """
+        R, S = sign
+        z = int(my_sha1(input_str), 16)
+        P = cls.curve_add(cls.curve_mul(cls.g, cls.inv(S, cls.order) * z),
+                          cls.curve_mul(key, cls.inv(S, cls.order) * R))
+        if P[0] == R:
+            return True
+        else:
+            return False
---
+++ sha256.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+import struct
+
+MOD = 0xFFFFFFFF
+
+# 寄存器初始值
+H = H0 = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+          0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]
+
+# 加法常量 K (64)
+K = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+     0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+     0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+     0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+     0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+     0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+     0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+     0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+     0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+     0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+     0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+     0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
+
+
+def right_rotate(data: int, k: int) -> int:
+    """
+    sha256 内部函数，返回循环右移结果。
+
+    :param data: 被移动的数字
+    :param k: 移动的步数
+    :return: 移动后的数字
+    """
+    return ((data >> k) | (data << (32 - k))) & MOD
+
+
+def my_sha256_hash(input_data: bytes) -> None:
+    """
+    sha256 内部函数，对分组后的一组消息进行处理，长度 64 字节。
+
+    :param input_data: 输入的字节流，长度 64 字节
+    """
+    global H
+
+    # 创建信息集合 w，0-15 为初始值，后面 48 位为0，之后进行操作。
+    w = list(struct.unpack(">" + "I" * 16, input_data)) + ([0] * 48)
+    for i in range(16, 64):
+        s0 = right_rotate(
+            w[i - 15], 7) ^ right_rotate(w[i - 15], 18) ^ (w[i - 15] >> 3)
+        s1 = right_rotate(
+            w[i - 2], 17) ^ right_rotate(w[i - 2], 19) ^ (w[i - 2] >> 10)
+        w[i] = (w[i - 16] + s0 + w[i - 7] + s1) & MOD
+
+    # 初始化变量
+    a, b, c, d, e, f, g, h = H
+
+    # 压缩函数主循环
+    for i in range(64):
+        # print(a, b, c, d, e, f, g, h)
+        s1 = right_rotate(e, 6) ^ right_rotate(e, 11) ^ right_rotate(e, 25)
+        ch = (e & f) ^ ((~e) & g)
+        tmp1 = (h + s1 + ch + K[i] + w[i]) & MOD
+        s0 = right_rotate(a, 2) ^ right_rotate(a, 13) ^ right_rotate(a, 22)
+        maj = (a & b) ^ (a & c) ^ (b & c)
+        tmp2 = (s0 + maj) & MOD
+
+        h = g
+        g = f
+        f = e
+        e = (d + tmp1) & MOD
+        d = c
+        c = b
+        b = a
+        a = (tmp1 + tmp2) & MOD
+
+    # 更改目前哈希值
+    H = (H[0] + a) & MOD, (H[1] + b) & MOD, \
+        (H[2] + c) & MOD, (H[3] + d) & MOD, \
+        (H[4] + e) & MOD, (H[5] + f) & MOD, \
+        (H[6] + g) & MOD, (H[7] + h) & MOD
+
+
+def my_sha256(input_str: str, is_number: bool = False, is_hex: bool = False) -> str:
+    """
+    对输入字符串进行 sha256
+
+    :param input_str: 输入的字符串，如果为数字需提前转换为字符串格式。
+    :param is_number: 是否按照数字格式处理字符串，若不是则会使用对应字符值。
+    :param is_hex: 是否是十六进制数字串，若为 False 则按照十进制处理。
+    :return: 长度为 64 的十六进制字符串，代表 sha256 后结果
+    """
+    if is_number:
+        # original_len = len(input_str)
+        original_len = 64
+        if is_hex:
+            num_str = hex(int(input_str, 16))[2:]
+        else:
+            num_str = hex(int(input_str, 10))[2:]
+
+        if len(num_str) < original_len:
+            num_str = "0" * (original_len - len(num_str)) + num_str
+
+        data_len = struct.pack('>Q', len(num_str) * 4)
+        data = b''
+        for i in range(0, len(num_str), 2):
+            num = (int(num_str[i], 16) << 4)
+            if i + 1 < len(num_str):
+                num += int(num_str[i + 1], 16)
+            data += struct.pack('<B', num)
+    else:
+        # 消息填充
+        # len() 返回的是字符数（英文 8 比特，1 字节），所有方法都应是 8 倍
+        data_len = struct.pack('>Q', len(input_str) * 8)
+        data = input_str.encode()
+
+    # 添加 10 序列
+    data += b'\x80'
+    data += b'\x00' * ((56 - len(data) % 64 + 64) % 64)
+    # 添加消息长度
+    data += data_len
+
+    # MD 缓冲区初始化
+    global H
+    H = H0
+
+    # 处理消息
+    # data 以 8 个字符为一个单位，每次处理 data[p:p+63]
+    p = 0
+    while p < len(data):
+        my_sha256_hash(data[p: p + 64])
+        p += 64
+
+    # 最终 H 连接即为答案
+    # print(result)
+    return "{:08x}".format(H[0]) + "{:08x}".format(H[1]) + \
+           "{:08x}".format(H[2]) + "{:08x}".format(H[3]) + \
+           "{:08x}".format(H[4]) + "{:08x}".format(H[5]) + \
+           "{:08x}".format(H[6]) + "{:08x}".format(H[7])
